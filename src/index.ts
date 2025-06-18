7dad2f33dc5b66ec2fa63514812ca666ef899de2
Arijit Debnath <itsarijitdebnath@gmail.com>
2025-06-18 14:45:12 +0530

    Implement user authentication with Prisma, JWT, and bcrypt; add environment configuration

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import env from "./lib/schemas/env";
 import authRouter from "./routes/auth.route";
 
 const app = express();
+
+app.use(express.json());
 app.use(morgan("dev"));
 
 // Routes
 app.use("/auth", authRouter);
 
-app.listen(env?.PORT || 3000, () => {
-  console.log(`Server running on PORT: ${3000}`);
+const port = env?.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Server running on PORT: ${port}`);
 });
--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 import authSchema from "../lib/schemas/auth";
+import prisma from "lib/db";
+import env from "lib/schemas/env";
 
 const authRouter = Router();
 
@@ -29,6 +33,68 @@ authRouter.post("/user/signup", async (req, res) => {
     avatarUrl,
     location,
   } = data;
+
+  const userExists = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (userExists) {
+    res.status(400).json({
+      success: false,
+      error: "This email is already in use",
+    });
+
+    return;
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await prisma.user.create({
+    data: {
+      first_name,
+      last_name,
+      designation,
+      department,
+      email,
+      password: hashedPassword,
+      location,
+      avatarUrl,
+    },
+  });
+
+  if (newUser.id) {
+    const tokenData = {
+      id: newUser.id,
+      email: newUser.email,
+    };
+
+    try {
+      const token = await jwt.sign(tokenData, env?.JWT_SECRET!, {
+        expiresIn: "7d",
+      });
+
+      res.cookie("dit-cookie", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+      });
+
+      res.json({
+        success: true,
+        message: "User signed up successfully",
+      });
+
+      return;
+    } catch (error) {
+      res.status(500).json({
+        success: false,
+        error,
+      });
+    }
+  }
 });
 
 export default authRouter;
